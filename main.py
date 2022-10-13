91b5f2ad0e032589456b9163a909b89dac7dd8ef
Angela <68617720+Angela3214@users.noreply.github.com>
2022-10-13 19:57:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,55 @@
-import threading
+import grid
+import minmax
 
-cv = threading.Condition()
-current_value = 1
+def computer_step(g):
+    n = g.n #Лучше назвать size например
+    for i in range(n): #Вместо i, j лучше использовать названия например row, col
+        for j in range(n):
+            if g.get(i, j) == grid.CELL_EMPTY:
+                g.set(i, j, grid.CELL_COMPUTER)
+                result = minmax.player_wins(g)
+                if not result:
+                    return
+                g.set(i, j, grid.CELL_EMPTY)
+    for i in range(n): #аналогично
+        for j in range(n):
+            if g.get(i, j) == grid.CELL_EMPTY:
+                g.set(i, j, grid.CELL_COMPUTER)
+                return
 
-def thread_function(i, n): # Лучше называть переменные в соответствии с их значениями (речь про n). Например,
-    #def thread_function(result, iteration_count):
-    global cv
-    global current_value
+def player_step(g):
+    print(g.draw())
+    while True: #Лучше использовать флаги вместо while true (например пока флаг true)
+        i, j = list(map(int, input("Enter row and column: ").split(' ')))
+        i, j = i - 1, j - 1
+        if g.valid_coords(i, j) and g.get(i, j) == grid.CELL_EMPTY:
+            g.set(i, j, grid.CELL_PLAYER)
+            break #Тогда и break тут не пригодится
+        else:
+            print('Invalid coordinates!')
 
-    for j in range(n): # Тогда здесь for iteration in range(iteration_count):
-        with cv:
-            cv.wait_for(lambda: current_value == i) #cv.wait_for(lambda: current_value == result)
-            print(i, end='') #print(result, end='')
-            current_value = 1 + current_value % 3
-            cv.notify_all()
+def main():
+    n = int(input("Enter grid size: ")) # Лучше использовать более понятные названия переменных: grid_size
+    g = grid.Grid(n) #Тогда здесь лучше grid_class_obj
+    empty_cells = n * n #grid_size * grid_size
+    # Лучше использовать флаги вместо while true (например пока флаг true)
+    while True: #Лучше ине дублировать код, а использовать else и флаги
+        computer_step(g) #Имя переменной (как указано выше)
+        if g.who_won() == grid.COMPUTER_WON:
+            print('Computer won!')
+            return
+        empty_cells -= 1
+        if empty_cells == 0:
+            print('Neither won!')
+            return
+        player_step(g)
+        if g.who_won() == grid.PLAYER_WON:
+            print('Player won!')
+            return
+        empty_cells -= 1
+        if empty_cells == 0:
+            print('Neither won!')
+            return
 
-n = int(input()) # iteration_count = int(input()). Лучше использовать функции (то есть засунуть это и следующий (ниже) код в функцию main и
-#добавить ниже if __name__ == "__main__": main() ) 
-
-threads = [
-    threading.Thread(target=thread_function, args=(1, n)), #Аналогично заменить n на, например, iteration_count
-    threading.Thread(target=thread_function, args=(2, n)),
-    threading.Thread(target=thread_function, args=(3, n)),
-]
-
-for thread in threads:
-    thread.start()
-
-for thread in threads:
-    thread.join()
+if __name__ == '__main__':
+    main()
